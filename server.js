57183406a25483c43c0045aad4dcb77eb179ac51
ZubtsovGit <dania.zubtsov@gmail.com>
2024-04-13 14:31:52 +0300

    server file path fixed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 
 // Serve index.html on the root path
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 app.listen(PORT, () => {
